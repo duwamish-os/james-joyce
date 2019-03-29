d19f0472a2747b8932cdf342b1a79aa23eaa3f0f
Upadhyay <prayag.upd@gmail.com>
2019-03-28 21:47:41 -0700

    refactor to use amazon hosted elasticsearch

---
+++ ulysses-logevents-aggregator/startup.sh
@@ -0,0 +1,10 @@
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.0.tar.gz
+
+## install elastalert
+sudo yum install -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
+
+sudo yum -y install python-setuptools
+sudo yum -y install python-pip
+
+sudo pip install elastalert
+
